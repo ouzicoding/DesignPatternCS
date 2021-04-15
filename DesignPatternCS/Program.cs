decefcc0688e0e463598f0785c19e0d688859107
欧子 <ouzi7997@163.com>
2021-04-15 14:13:44 +0800

    1.2

--- DesignPatternCS/Program.cs
+++ DesignPatternCS/Program.cs
@@ -7,22 +7,27 @@ namespace DesignPatternCS
         static void Main(string[] args)
         {
             Console.Write("请输入数字A:");
+            //1.ABCD命名不规范
             string A = Console.ReadLine();
             Console.Write("请选择运算符号（+ - * /）：");
             string B = Console.ReadLine();
             Console.Write("请输入数字B:");
             string C = Console.ReadLine();
             string D = "";
+            //2. if浪费资源
             if (B == "+")
+                //4.方法重复 代码冗余
                 D = Convert.ToString(Convert.ToDouble(A) + Convert.ToDouble(C));
             if (B == "-")
                 D = Convert.ToString(Convert.ToDouble(A) - Convert.ToDouble(C));
             if (B == "*")
                 D = Convert.ToString(Convert.ToDouble(A) * Convert.ToDouble(C));
             if (B == "/")
+                //3. 数据未校验 （除数0）
                 D = Convert.ToString(Convert.ToDouble(A) / Convert.ToDouble(C));
 
             Console.WriteLine("结果是：" + D);
         }
     }
 }
+
