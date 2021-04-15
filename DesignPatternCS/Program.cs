1c1c0cefc89aedae6f9e273ab8637c34874b3911
欧子 <ouzi7997@163.com>
2021-04-15 13:47:49 +0800

    1.1

--- DesignPatternCS/Program.cs
+++ DesignPatternCS/Program.cs
@@ -6,7 +6,23 @@ namespace DesignPatternCS
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("请输入数字A:");
+            string A = Console.ReadLine();
+            Console.Write("请选择运算符号（+ - * /）：");
+            string B = Console.ReadLine();
+            Console.Write("请输入数字B:");
+            string C = Console.ReadLine();
+            string D = "";
+            if (B == "+")
+                D = Convert.ToString(Convert.ToDouble(A) + Convert.ToDouble(C));
+            if (B == "-")
+                D = Convert.ToString(Convert.ToDouble(A) - Convert.ToDouble(C));
+            if (B == "*")
+                D = Convert.ToString(Convert.ToDouble(A) * Convert.ToDouble(C));
+            if (B == "/")
+                D = Convert.ToString(Convert.ToDouble(A) / Convert.ToDouble(C));
+
+            Console.WriteLine("结果是：" + D);
         }
     }
 }
